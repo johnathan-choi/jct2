8a6499a55424de0ce40b330cb09de91dc2c2d9ee
Johnathan Choi <john.choi.447@gmail.com>
2017-09-19 13:06:24 -0700

    more core.js work

--- public/core.js
+++ public/core.js
@@ -1,9 +1,17 @@
 // public/core.js
 
-var app = angular.module('jct2', []);  
-app.controller('mainController', function($scope){
+var app = angular.module('jct', []);  
+app.controller('createTicketPage', function($scope, $http){
     $scope.createTicket = function(){
-        alert(1);
-        $scope.formBody = "test";
-    }
+        var submitData = [
+            $scope.formSubject,
+            $scope.formTime,
+            $scope.formBody
+        ];
+        // for(var i=0; i<submitData.length; i++){
+        //     alert(submitData[i]);
+        // }
+        $http.post('/api/tickets', submitData)
+            .then(alert("POST success"), alert("POST failure"));
+    };
 });
